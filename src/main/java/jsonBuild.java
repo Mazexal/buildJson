ad7413ab8f168de1f68322d05948051eee67064a
Mazexal <1023520044@qq.com>
2017-10-11 22:05:27 +0800

    修复

--- src/main/java/jsonBuild.java
+++ src/main/java/jsonBuild.java
@@ -69,6 +69,7 @@ public class jsonBuild {
             if(i!=length-1){
                 stringBuilder.append(",");
             }
+            
         }
         stringBuilder.append("}");
         return (stringBuilder.toString());
